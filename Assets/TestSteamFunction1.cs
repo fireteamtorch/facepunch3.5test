fc0d73d836d2ad02b40b5ef80fc526df63670428
Jeffrey Hsu <silverhsu@gmail.com>
2018-06-20 18:49:12 -0500

    testing remote file attachments

--- Assets/TestSteamFunction1.cs
+++ Assets/TestSteamFunction1.cs
@@ -9,6 +9,8 @@ public class TestSteamFunction1 : MonoBehaviour {
 
     public Facepunch.Steamworks.Leaderboard testLeaderboard;
 
+    public string outputFileString;
+
 	// Use this for initialization
 	void Start () {
         SteamManager.StartClient();
@@ -27,6 +29,23 @@ public class TestSteamFunction1 : MonoBehaviour {
         // Add the shit
         if (Input.GetKeyDown(KeyCode.J))
         {
+            
+            const string attachment = "123 facepunch! \n testing \t testlkjlkaegjl \n \n aflkhlaghjla";
+            var tempRemoteFile = Facepunch.Steamworks.Client.Instance.RemoteStorage.CreateFile("remoteVideo.mp4");
+            //file.WriteAllText(attachment);
+
+            byte[] videoBytes = System.IO.File.ReadAllBytes("Assets/testVideoBB.mp4");
+            Debug.Log(videoBytes.Length);
+
+            byte[] miniArray = new byte[3000];
+            System.Array.Copy(videoBytes, 0, miniArray, 0, miniArray.Length);
+
+            tempRemoteFile.WriteAllBytes(miniArray);
+
+            bool tempAttachBool = testLeaderboard.AttachRemoteFile(tempRemoteFile, null, null);
+            Debug.Log("Attach: " + tempAttachBool.ToString());
+            
+
             Debug.Log("Trying to add score");
             int num2add = 20;
             int[] shitToAdd = new int[num2add];
@@ -35,14 +54,17 @@ public class TestSteamFunction1 : MonoBehaviour {
                 shitToAdd[i] = int.MaxValue - i;
             }
             // bool isSuccess = testLeaderboard.AddScore(false, 500, 1,2,33,4,5);
-            bool isSuccess = testLeaderboard.AddScore(false, -100, shitToAdd, null, null);
+            bool isSuccess = testLeaderboard.AddScore(false, -500, shitToAdd, null, null);
             Debug.Log("Add success: " + isSuccess);
-            //uint datuint = 4294967291;
-            //int datint = -5;
+            /*
+            uint datuint = 4294967291;
+            int datint = -5;
             uint datuint = uint.MaxValue;
             int datint = int.MinValue;
             Debug.Log("uint = " + datuint + ", cast int = " + (int)datuint);
             Debug.Log("int = " + datint + ", cast uint = " + (uint)datint);
+            */
+
         }
 
         if (Input.GetKeyDown(KeyCode.A))
@@ -54,7 +76,12 @@ public class TestSteamFunction1 : MonoBehaviour {
                 Debug.Log(testLeaderboard.Name + ", total entries:" + testLeaderboard.TotalEntries);
                 foreach (var tempVar in testLeaderboard.Results)
                 {
-                    Debug.Log(" rank: " + tempVar.GlobalRank + " name: " + tempVar.Name + " score: " + tempVar.Score + " ID: " + tempVar.SteamId);
+                    Debug.Log("rank: " + tempVar.GlobalRank + "\tname: " + tempVar.Name + "\tscore: " + tempVar.Score + "\tID: " + tempVar.SteamId);
+                    if(tempVar.AttachedFile != null)
+                    {
+                        Debug.Log(tempVar.AttachedFile.FileName);
+                    }
+                    /*
                     if (tempVar.SubScores != null)
                     {
                         foreach (int currSubScore in tempVar.SubScores)
@@ -62,6 +89,56 @@ public class TestSteamFunction1 : MonoBehaviour {
                             Debug.Log("Subscore for " + tempVar.Name + ": " + currSubScore);
                         }
                     }
+                    */
+                }
+            }
+        }
+
+        Facepunch.Steamworks.Leaderboard.Entry tempEntry;
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            foreach (var tempVar in testLeaderboard.Results)
+            {
+                if(tempVar.Name == "Silverhsu")
+                {
+                    //tempVar.AttachedFile.dow
+                    tempEntry = tempVar;
+                    tempVar.AttachedFile.Download(null, null);
+                    Debug.Log("Starting download");
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            foreach (var tempVar in testLeaderboard.Results)
+            {
+                if (tempVar.Name == "Silverhsu")
+                {
+                    //tempVar.AttachedFile.dow
+                    //tempEntry = tempVar;
+                    //tempVar.AttachedFile.Download(null, null);
+                    int tempDownloadedBytes;
+                    int tempTotalBytes;
+
+                    tempVar.AttachedFile.GetDownloadProgress(out tempDownloadedBytes, out tempTotalBytes);
+                    Debug.Log(tempDownloadedBytes + " " + tempTotalBytes);
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            foreach (var tempVar in testLeaderboard.Results)
+            {
+                if (tempVar.Name == "Silverhsu")
+                {
+                    //tempVar.AttachedFile.dow
+                    //outputFileString = tempVar.AttachedFile.ReadAllText(null);
+                    //Debug.Log("file:" + outputFileString);
+                    outputFileString = tempVar.AttachedFile.FileName;
+                    Debug.Log(tempVar.AttachedFile.FileName + "  " + tempVar.AttachedFile.SizeInBytes + " " + tempVar.AttachedFile.SharingId);
                 }
             }
         }

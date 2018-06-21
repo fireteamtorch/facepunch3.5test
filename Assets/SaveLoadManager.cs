a6011ab082c95e6961ad181a9eeee20f182dc1a8
fireteamtorch <fireteam_torch@yahoo.com>
2018-06-21 02:05:05 -0500

    added a test saving and loading system with serializable

---
+++ Assets/SaveLoadManager.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Collections.Generic;
+
+public static class SaveLoadManager {
+
+    public static string name = "jeff";
+
+    private static char fileSep = Path.DirectorySeparatorChar;
+    private static string saveFilePath = Application.persistentDataPath + fileSep + "save";
+    private static string saveFileName = "savefile_" + name + ".dat";
+
+    // Data to save
+    public static BandInfo band = null;
+    public static string[] setList = new string[] {"wtf", "bbq"};
+    public static List<float> reviews = new List<float>();
+
+    public static void saveAllData()
+    {
+        Debug.Log("Saving to " + saveFilePath + fileSep + saveFileName + "...");
+
+        if (!Directory.Exists(saveFilePath))
+        {
+            Directory.CreateDirectory(saveFilePath);
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(saveFilePath + fileSep + saveFileName);
+
+        SaveData data = new SaveData();
+        data.band = band;
+        data.setList = setList;
+        data.reviews = reviews;
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public static void loadAllData()
+    {
+        Debug.Log("Loading from " + saveFilePath + fileSep + saveFileName + "...");
+
+        if (File.Exists(saveFilePath + fileSep + saveFileName))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(saveFilePath + fileSep + saveFileName, FileMode.Open);
+            SaveData data = (SaveData) bf.Deserialize(file);
+            file.Close();
+
+            band = data.band;
+            setList = data.setList;
+            reviews = data.reviews;
+        } else
+        {
+            Debug.Log("No file exists of that name.");
+        }
+    }
+
+    public static void resetVariables()
+    {
+        band = null;
+        setList = new string[] { "wtf", "bbq" };
+        reviews = new List<float>();
+    }
+
+    public static void printVariables()
+    {
+        if (band == null)
+        {
+            Debug.Log("Band: null");
+        } else
+        {
+            Debug.Log("Band: " + band.ToString());
+        }
+        foreach(string currSong in setList)
+        {
+            Debug.Log("Set list contains: " + currSong);
+        }
+        if (reviews.Count == 0)
+        {
+            Debug.Log("Reviews are empty.");
+        } else
+        {
+            foreach (float currReview in reviews)
+            {
+                Debug.Log("Reviews contains score: " + currReview);
+            }
+        }
+    }
+
+    public static void setName(string newName)
+    {
+        name = newName;
+        saveFileName = "savefile_" + name + ".dat";
+        Debug.Log("Name is now " + name);
+    }
+
+}
+
+[Serializable]
+public class SaveData
+{
+
+    public BandInfo band;
+    public string[] setList;
+    public List<float> reviews;
+
+    public SaveData()
+    {
+        
+    }
+
+}
+
+[Serializable]
+public enum EInstrumentType { Guitar, Bass, Drums, Piano, Vocals}
+
+[Serializable]
+public class BandInfo
+{
+
+    public string name;
+    public int rank;
+    public EInstrumentType[] instruments;
+
+    public BandInfo(string name, int rank, EInstrumentType[] instruments)
+    {
+        this.name = name;
+        this.rank = rank;
+        this.instruments = instruments;
+    }
+
+    public override string ToString()
+    {
+        string toReturn = "";
+        toReturn += "name: ";
+        toReturn += name;
+        toReturn += ", rank: ";
+        toReturn += rank;
+        toReturn += ", instruments: ";
+        foreach (EInstrumentType currInst in instruments)
+        {
+            toReturn += currInst;
+            toReturn += ", ";
+        }
+        return toReturn;
+    }
+
+}
---
+++ Assets/TestSaveLoadScript.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestSaveLoadScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SaveLoadManager.resetVariables();
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SaveLoadManager.printVariables();
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            BandInfo newBand = new BandInfo("CollaboDaisakusen", 100, new EInstrumentType[] { EInstrumentType.Guitar, EInstrumentType.Guitar, EInstrumentType.Bass, EInstrumentType.Drums, EInstrumentType.Piano, EInstrumentType.Vocals, EInstrumentType.Vocals });
+            string[] newSetList = new string[] {"Ambiguous", "Startear", "Button", "Golden Life" };
+            List<float> newReviews = new List<float>();
+            newReviews.Add(Random.Range(0f, 1f));
+            newReviews.Add(Random.Range(0f, 1f));
+            newReviews.Add(Random.Range(0f, 1f));
+            SaveLoadManager.band = newBand;
+            SaveLoadManager.setList = newSetList;
+            SaveLoadManager.reviews = newReviews;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SaveLoadManager.saveAllData();
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            SaveLoadManager.loadAllData();
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (SaveLoadManager.name == "jeff")
+            {
+                SaveLoadManager.setName("david");
+            } else if (SaveLoadManager.name == "david")
+            {
+                SaveLoadManager.setName("yi");
+            } else if (SaveLoadManager.name == "yi")
+            {
+                SaveLoadManager.setName("jeff");
+            }
+        }
+
+    }
+}
